4bd05c31b3c30dd422b3514e57aac4e572bd873a
Sheng Chih Su <preztons@gmail.com>
2020-06-02 15:36:05 +0800

    pleaseeeeee

--- idk-test.py
+++ idk-test.py
@@ -1,16 +1,9 @@
 wow = 'wow'
 
 def test_branch():
-
     pass
 
-L = [87, 73, 78, 68, 79, 87, 83, 32, 73, 83, 32, 83, 79, 32, 71, 79, 79, 68]
+L = [87,73,78,68,79,87,83,32,73,83,32,83,79,32,71,79,79,68]
 for i in L:
     print(chr(i))
-<<<<<<< HEAD
 
-
-def letsGitGud():
-    pass
-=======
->>>>>>> b73b128ec8d4ad47eafcbba34ddab582e5fefb9b
