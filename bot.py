84ad1a547e1cc7fe5380e68c9dc8b9e2bff058a3
zel9689 <57132523+Zel9689@users.noreply.github.com>
2020-06-06 15:31:18 +0800

    command: msghere; 用一個json存server和user的設定

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 motto_lib = os.path.join(PROJECT_ROOT, 'motto.json')
+info = os.path.join(PROJECT_ROOT, 'server-info.json')
 
 env_path = os.path.join(PROJECT_ROOT, '.env')
 load_dotenv(dotenv_path=env_path, override=True)
@@ -17,6 +18,24 @@ bot = commands.Bot(command_prefix='js ')
 # 資料庫從這邊讀進去
 with open(motto_lib, 'r', encoding='utf8') as read_motto:
     jMotto = json.load(read_motto)
+
+
+# 用來修改server-info中的資料
+def info_set(obj, ID, data):  # obj=json第一層的key; ID=東西的ID; data=用dict的格式傳進來
+    with open(info, 'r') as read_info:
+        jinfo = json.load(read_info)
+    flag = False
+    for i in jinfo[obj]:
+        if(i["ID"] == ID):  # 原本就有這筆資料則更新就好
+            flag = True
+            i.update(data)
+            break
+    if(flag is False):  # 沒有這筆資料要新增
+        x = {"ID": ID}
+        x.update(data)
+        jinfo[obj].append(x)
+    with open(info, 'w') as read_info:
+        json.dump(jinfo, read_info, indent=4)
 
 
 @bot.event
@@ -37,9 +56,16 @@ async def on_message(msg):
     if (msg.content.startswith('js ')):
         await bot.process_commands(msg)
 
+
 @bot.command()
-async def sendPic(ctx):
+async def sendPic(msg):
     pass
 
 
+@bot.command()
+async def msghere(msg):
+    guild_id = msg.message.guild.id
+    channel_id = msg.channel.id
+    x = {"ID": guild_id, "channel_set": channel_id}
+    info_set("server", guild_id, x)
 bot.run(TOKEN)
